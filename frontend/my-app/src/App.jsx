3ac0cfbf69ee0923989f9eb5b4e0a8a382a3b119
el-barae-ensi <elkhader.elbarae@ensi.ma>
2025-08-25 20:09:39 +0100

    Update App.jsx fix

--- frontend/my-app/src/App.jsx
+++ frontend/my-app/src/App.jsx
@@ -1,16 +1,5 @@
 
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Login from './pages/Login';
-
-function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Login />} />
-      </Routes>
-    </BrowserRouter>
-=======
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Login from "./pages/Login";
 import Dashboard from "./layouts/Dashboard";
